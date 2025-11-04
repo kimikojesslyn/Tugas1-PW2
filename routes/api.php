d7a344d1be5e769dbb6a06bcc85fbc3a322e7fbc
Jesslyn Kimiko <jesslynkimiko_2428250005@mhs.mdp.ac.id>
2025-11-04 14:39:34 +0700

    0

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,23 @@ Route::get('/user', function (Request $request) {
 Route::get('/penulis', [PenulisController::class, 'index']);
 Route::get('/blog', [BlogController::class, 'index']);
 
-Route::post('/penulis', [PenulisController::class,'store'])->middleware('auth:sanctum');
-Route::post('/blog', [BlogController::class,'store'])->middleware('auth:sanctum');;
+Route::post('/penulis', [PenulisController::class, 'store']);
+Route::post('/blog', [BlogController::class, 'store']);
 
-Route::patch('/penulis/{id}', [PenulisController::class,'update'])->middleware('auth:sanctum');;
-Route::patch('/blog/{id}', [BlogController::class,'update'])->middleware('auth:sanctum');;
+Route::patch('/penulis/{id}', [PenulisController::class, 'update']);
+Route::patch('/blog/{id}', [BlogController::class, 'update']);
 
-Route::delete('/penulis/{id}', [PenulisController::class,'destroy'])->middleware('auth:sanctum');;
-Route::delete('/blog/{id}', [BlogController::class,'destroy'])->middleware('auth:sanctum');;
+Route::delete('/penulis/{id}', [PenulisController::class, 'destroy']);
+Route::delete('/blog/{id}', [BlogController::class, 'destroy']);
+
+// Route::post('/penulis', [PenulisController::class,'store'])->middleware('auth:sanctum');
+// Route::post('/blog', [BlogController::class,'store'])->middleware('auth:sanctum');;
+
+// Route::patch('/penulis/{id}', [PenulisController::class,'update'])->middleware('auth:sanctum');;
+// Route::patch('/blog/{id}', [BlogController::class,'update'])->middleware('auth:sanctum');;
+
+// Route::delete('/penulis/{id}', [PenulisController::class,'destroy'])->middleware('auth:sanctum');;
+// Route::delete('/blog/{id}', [BlogController::class,'destroy'])->middleware('auth:sanctum');;
 
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
